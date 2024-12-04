02b682bf808febd3bce951bae043c23ef84d7bd8
ContactKushik <127011605+ContactKushik@users.noreply.github.com>
2024-12-04 10:19:03 +0530

    port dynamically added

--- app.js
+++ app.js
@@ -133,6 +133,6 @@ io.on("connection", (socket) => {
 // Render the homepage
 
 // Start the server
-server.listen(3000, () => {
+server.listen(process.env.PORT||3000, () => {
   console.log("Server is running on port 3000: http://localhost:3000");
 });
